98e903a4ea6a427b8a3b48965488e76f6d759ce9
Hack Reactor Students <accounts@hackreactor.com>
2013-03-20 16:29:18 -0700

    Finished implementation fo getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,4 +1,19 @@
 // fixme
 var getElementsByClassName = function(className){
-  return document.getElementsByClassName(className);
+  var regex = new RegExp('(^|\\b)' + className + '(\\b|$)');
+  var output = [];
+
+  var getChildElementsByClassName = function(node, className) {
+    if(node.hasChildNodes()) {
+      var children = node.children;
+      for(var i=0; i < children.length; i++) {
+        getChildElementsByClassName(children[i], className);
+      }
+    }
+    if(node.className.match(regex)) {
+      output.push(node);
+    }
+  }
+  getChildElementsByClassName(document.body);
+  return output;
 }
