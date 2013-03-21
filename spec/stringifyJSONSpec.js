e4a668cf2f67ead06e7452db3d3492b9b48cdbe1
Hack Reactor Students <accounts@hackreactor.com>
2013-03-20 17:32:35 -0700

    Basic JSON stringify functionality works

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -1,11 +1,45 @@
 // test cases are described in fixtures.js
 describe("stringifyJSON", function(){
+  describe("basic types", function() {
+    it("properly stringifies a string", function() {
+      expect(JSON.stringify('test')).toEqual(stringifyJSON('test'));
+    });
+    it("properly stringifies undefined and null values", function() {
+      expect(JSON.stringify(null)).toEqual(stringifyJSON(null));
+      expect(JSON.stringify(undefined)).toEqual(stringifyJSON(undefined));
+    });
+    it("properly stringifies a boolean", function() {
+      expect(JSON.stringify(true)).toEqual(stringifyJSON(true));
+      expect(JSON.stringify(false)).toEqual(stringifyJSON(false));
+    });
+    it("properly stringifies an int", function() {
+      expect(JSON.stringify(123)).toEqual(stringifyJSON(123));
+      expect(JSON.stringify(12.3)).toEqual(stringifyJSON(12.3));
+      expect(JSON.stringify(-123)).toEqual(stringifyJSON(-123));
+    });
+    it("properly stringifies an array", function() {
+      expect(JSON.stringify([1,2,3])).toEqual(stringifyJSON([1,2,3]));
+      expect(JSON.stringify([1,'test',3])).toEqual(stringifyJSON([1,'test',3]));
+      expect(JSON.stringify([1,2,true])).toEqual(stringifyJSON([1,2,true]));
+    });
+
+    // Switching expectant and result paramaters (because we're idiots)
+    it("properly stringifies an object", function() {
+      expect(stringifyJSON({a:1,b:'test'})).toEqual(JSON.stringify({a:1,b:'test'}));
+    });
+    it("properly stringifies a function", function() {
+      var testFunc = function() { return 10*12; };
+      expect(stringifyJSON(testFunc)).toEqual(JSON.stringify(testFunc));
+    });
+  });
+
   it("should match the result of calling JSON.stringify", function(){
 
     validStrings.forEach(function(test){
       var obj = JSON.parse(test);
       var result = stringifyJSON(obj);
       var expected = JSON.stringify(obj);
+      console.log(obj);
       expect(result).toEqual(expected);
     });
 
@@ -15,5 +49,5 @@ describe("stringifyJSON", function(){
       expect(result).toEqual(expected);
     });
 
-  })
+  });
 });
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,1 +1,59 @@
-var stringifyJSON = JSON.stringify; // fixme
+var stringifyJSON = function(input) {
+
+  var functionStringify = function(func) {
+
+  };
+
+  // Stringify method to parse out array objects
+  var arrayStringify = function(array) {
+    // Return empty array string
+    if(array.length === 0) return '[]';
+    var result = [];
+
+    // Iterate through every element and call stringifyJSON recursively
+    for(var i = 0; i < array.length; i++) {
+      result.push(stringifyJSON(array[i]));
+    }
+    return '[' + result.join(',') + ']';
+  };
+
+  // Stringify method to parse out objects
+  var objectStringify = function(object) {
+    // Return empty object string
+    if(Object.keys(object).length === 0) return '{}';
+    var result = '{';
+
+    // Iterate through every element and call stringifyJSON recursively
+    for(key in object) {
+      if(object.hasOwnProperty(key)) {
+        result += stringifyJSON(key) + ':' + stringifyJSON(object[key]) + ',';
+      }
+    }
+    // Strip off the last comma and add a closing bracket
+    return result.slice(0,-1) + '}';
+  };
+
+  // Primitive types return toString() method or null
+  if (typeof input === 'string') {
+    return "\"" + input + "\"";
+  } else if (typeof input === 'number') {
+    return input.toString();
+  } else if (typeof input === 'boolean') {
+    return input.toString();
+  } else if (input === null) {
+    return 'null';
+  } else if (typeof input === 'undefined') {
+    return undefined;
+  } else if (input instanceof Array) {
+    return arrayStringify(input);
+  } else if (typeof input === 'object') {
+    return objectStringify(input);
+  }
+
+   else if (typeof input === 'function') {
+    return functionStringify(input);
+  } else {
+    throw new Error('WTF is this?');
+  }
+
+}
